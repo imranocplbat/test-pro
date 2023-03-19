bb55f69747ebef3d089cb58ab8bb556c75af9caa
Shakir-Ahammed <shuvocsevu231@gmail.com>
2023-03-19 13:26:33 +0600

    question set

---
+++ question.php
@@ -0,0 +1,324 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <title>Question</title>
+</head>
+
+<body>
+    <div class="container text-center mt-2">
+        <h1>Question</h1>
+        <p>Note: This exam will be held for next 30 minutes.</p>
+
+        <div class="d-flex justify-content-center ">
+            <div class="row ">
+                <div class="col-md-12">
+                    <form action="submit.php" method="post">
+
+
+
+                        <div class="form-group">
+
+
+                            <table class="table table-bordered">
+                                <tr>
+
+                                    <th>
+
+                                        <label for="question1">1. What is topological sorting ?</label>
+                                    </th>
+
+
+                                </tr>
+                                <tbody>
+
+                                    <tr>
+
+
+                                        <td>
+                                            <div>
+
+                                                <input type="radio" class="" value='1' name="question1">
+                                                <label for="Status">1. It is a searching Algorithm</label>
+                                            </div>
+                                        </td>
+                                    </tr>
+                                    <tr>
+
+                                        <td>
+                                            <div>
+                                                <input type="radio" class="" value='2' name="question1">
+                                                <label for="status-2">2. It is a linear sorting Algorithm</label>
+                                            </div>
+                                        </td>
+                                    </tr>
+                                    <tr>
+
+                                        <td>
+                                            <div>
+                                                <input type="radio" class="" value='3' name="question1">
+                                                <label for="status-2">3. It is a non linear sorting Algorithm</label>
+                                            </div>
+                                        </td>
+                                    </tr>
+                                    <tr>
+
+
+                                        <td>
+
+                                            <div>
+                                                <input type="radio" class="" value='4' name="question1">
+                                                <label for="status-2">4. It is a time Efficient sorting Algorithm.</label>
+                                            </div>
+                                        </td>
+                                    </tr>
+                                </tbody>
+
+                            </table>
+                        </div>
+
+                        <table class="table table-bordered">
+                            <tr>
+
+                                <th>
+
+                                    <label>2. What is the fastest sorting Algorithm ?</label>
+                                </th>
+
+
+                            </tr>
+                            <tbody>
+
+                                <tr>
+
+
+                                    <td>
+                                        <div>
+
+                                            <input type="radio" class="" value='1' name="question2">
+                                            <label for="Status">1. Heap sort</label>
+                                        </div>
+                                    </td>
+                                </tr>
+                                <tr>
+
+                                    <td>
+                                        <div>
+                                            <input type="radio" class="" value='2' name="question2">
+                                            <label for="status-2">2. Bubble Sort</label>
+                                        </div>
+                                    </td>
+                                </tr>
+                                <tr>
+
+                                    <td>
+                                        <div>
+                                            <input type="radio" class="" value='3' name="question2">
+                                            <label for="status-2">3. Marge Sort</label>
+                                        </div>
+                                    </td>
+                                </tr>
+                                <tr>
+
+
+                                    <td>
+
+                                        <div>
+                                            <input type="radio" class="" value='4' name="question2">
+                                            <label for="status-2">4. Quick Sort</label>
+                                        </div>
+                                    </td>
+                                </tr>
+                            </tbody>
+
+                        </table>
+
+                        <table class="table table-bordered">
+                            <tr>
+
+                                <th>
+
+                                    <label for="question4">3. What is the Faster Programming Language ?</label>
+                                </th>
+
+
+                            </tr>
+                            <tbody>
+
+                                <tr>
+
+
+                                    <td>
+                                        <div>
+
+                                            <input type="radio" class="" value='1' name="question3">
+                                            <label for="Status">1. C++</label>
+                                        </div>
+                                    </td>
+                                </tr>
+                                <tr>
+
+                                    <td>
+                                        <div>
+                                            <input type="radio" class="" value='2' name="question3">
+                                            <label>2. Python</label>
+                                        </div>
+                                    </td>
+                                </tr>
+                                <tr>
+
+                                    <td>
+                                        <div>
+                                            <input type="radio" class="" value='3' name="question3">
+                                            <label>3. Java</label>
+                                        </div>
+                                    </td>
+                                </tr>
+                                <tr>
+
+
+                                    <td>
+
+                                        <div>
+                                            <input type="radio" class="" value='4' name="question3">
+                                            <label for="status-2">4. C#</label>
+                                        </div>
+                                    </td>
+                                </tr>
+                            </tbody>
+
+                        </table>
+
+                        <table class="table table-bordered">
+                            <tr>
+
+                                <th>
+
+                                    <label for="question1">4. what is non linear Data structure ?</label>
+                                </th>
+
+
+                            </tr>
+                            <tbody>
+
+                                <tr>
+
+
+                                    <td>
+                                        <div>
+
+                                            <input type="radio" class="" value='1' name="question4">
+                                            <label for="Status">1. Array</label>
+                                        </div>
+                                    </td>
+                                </tr>
+                                <tr>
+
+                                    <td>
+                                        <div>
+                                            <input type="radio" class="" value='2' name="question4">
+                                            <label for="status-2">2. Graph</label>
+                                        </div>
+                                    </td>
+                                </tr>
+                                <tr>
+
+                                    <td>
+                                        <div>
+                                            <input type="radio" class="" value='3' name="question4">
+                                            <label for="status-2">3. Stack</label>
+                                        </div>
+                                    </td>
+                                </tr>
+                                <tr>
+
+
+                                    <td>
+
+                                        <div>
+                                            <input type="radio" class="" value='4' name="question4">
+                                            <label for="status-2">4. Queue.</label>
+                                        </div>
+                                    </td>
+                                </tr>
+                            </tbody>
+
+                        </table>
+
+                        <table class="table table-bordered">
+                            <tr>
+
+                                <th>
+
+                                    <label for="">5. Where run a javascript programe ?</label>
+                                </th>
+
+
+                            </tr>
+                            <tbody>
+
+                                <tr>
+
+
+                                    <td>
+                                        <div>
+
+                                            <input type="radio" class="" value='1' name="question5">
+                                            <label for="Status">1.Editor</label>
+                                        </div>
+                                    </td>
+                                </tr>
+                                <tr>
+
+                                    <td>
+                                        <div>
+                                            <input type="radio" class="" value='2' name="question5">
+                                            <label for="status-2">2. Compiler</label>
+                                        </div>
+                                    </td>
+                                </tr>
+                                <tr>
+
+                                    <td>
+                                        <div>
+                                            <input type="radio" class="" value='3' name="question5">
+                                            <label for="status-2">3. Browser</label>
+                                        </div>
+                                    </td>
+                                </tr>
+                                <tr>
+
+
+                                    <td>
+
+                                        <div>
+                                            <input type="radio" class="" value='4' name="question5">
+                                            <label for="status-2">4. Server</label>
+                                        </div>
+                                    </td>
+                                </tr>
+                            </tbody>
+
+                        </table>
+
+
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+
+
+    </div>
+
+
+</body>
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+
+</html>
